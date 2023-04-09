c59ebe29d2d3f6a52ac1ddd75aa1af4aa3e9236c
tanmay2626~ <tanmaywaykar17@gmail.com>
2023-04-09 20:32:46 +0530

    add signout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import jwt_decode from "jwt-decode";
-import './App.css'
+import './App.css';
 
 function App() {
+  const [ user, setUser ] = useState({});
 
   const handleCallbackResponse = (res) =>{
-    const user = jwt_decode(res.credential);
-    console.log(user);
+    const cred = jwt_decode(res.credential);
+    console.log(cred);
+    setUser(cred);
+    document.getElementById("signIn").hidden = true;
+  }
+
+  const handleSignOut = () =>{
+    setUser({});
+    document.getElementById("signIn").hidden = false;
   }
   
   useEffect(()=>{
@@ -27,6 +35,13 @@ function App() {
   return (
     <div className="App">
     <div id='signIn'></div>
+    {
+      Object.keys(user).length !=0 &&
+      <button onClick={handleSignOut}>Signout</button>
+    }
+    {
+      user && <h1>{user.name}</h1>
+    }
     </div>
   )
 }
